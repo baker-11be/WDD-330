ef9c91060f4c31b36e23e732acd796ce2666dd48
baker-11b <bakermfitumukiza-email5@example.com>
2026-09-11 11:54:58 +0300

    ADD

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,3 +21,10 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
+// --- NEW FUNCTION ADDED TO MATCH STEP 2 OF THE RUBRIC ---
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get(param);
+}
